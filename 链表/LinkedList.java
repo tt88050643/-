c5d6ff178e49c794fac447f265d68db559f8eb74
unknown <zhaimeng@ZHAIMENG-JSFWB.neeq.com.cn>
2015-07-19 16:31:49 +0800

    add linkedlist

---
+++ 链表/LinkedList.java
@@ -0,0 +1,64 @@
+public class LinkedList{
+	public Node first;
+	
+	/*
+	 *插入新节点，在头节点前插入，保持first为链表的第一个节点
+	 */
+	public void insertFirst(long value){
+		Node node = new Node(value);
+		node.next = first;
+		first = node;
+		
+	}
+	/*
+	 *删除一个节点，在头节点
+	 */
+	public Node deleteFirst(){
+		Node tmpNode = first;
+		first = first.next;
+		return tmpNode;
+	}
+	
+	/*
+	 *显示节点
+	 */
+	public void display(){
+		Node currentNode = first;
+		for(;currentNode!=null;currentNode=currentNode.next){
+			System.out.print(currentNode.data + " ");
+		}
+		System.out.println();
+	}
+	/*
+	 *查找节点
+	 */
+	public Node find(long value){
+		Node currentNode = first;
+		for(;currentNode!=null;currentNode=currentNode.next){
+			if(currentNode.data == value){
+				break;
+			}
+		}
+		return currentNode;
+	}
+	/*
+	 *根据值来删除node
+	 */
+	public Node delete(long value){
+		Node currentNode = first;
+		Node previous = first;
+		if(value == currentNode.data){
+			first = first.next;
+			return currentNode;
+		}
+		for(;currentNode!=null;previous = currentNode, currentNode=currentNode.next){
+			if(value == currentNode.data){
+				break;
+			}
+		}
+		
+		previous.next = currentNode.next;
+		return currentNode;
+		
+	}
+}
